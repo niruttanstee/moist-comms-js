6d86fa45cb69e31d73bf81e00387c854d8f85ff1
Nirutt <80709800+NiruttRP@users.noreply.github.com>
2021-11-06 22:22:54 +0000

    commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { token } = require('./config.json');
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
