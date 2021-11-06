6760d9ebb257f57dba78d502356233b86a6299b4
Nirutt <80709800+NiruttRP@users.noreply.github.com>
2021-11-06 18:26:48 +0000

    commit

--- startup.js
+++ startup.js
@@ -3,7 +3,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', message => {
