e090bb318c13e6f9c675c515edd2fd747cac3c18
Nirutt <80709800+NiruttRP@users.noreply.github.com>
2021-11-06 17:40:33 +0000

    New module

--- startup.js
+++ startup.js
@@ -1,15 +1,25 @@
 /* const means fixed */
+/* import Discord.js library */
 const Discord = require("discord.js")
+/* create client instance, a connection to discord*/
 const client = new Discord.Client()
 
+/* check for events, on ready so activates when
+the bot is ready to be used*/
 client.on("ready", () => {
     console.log('Logged in as ${client.user.tag}!')
 })
 
+/* when discord message event is called, client on message
+* and if the message content is ping
+* reply pong */
 client.on("message", msg => {
     if (msg.content === "ping") {
         msg.reply("pong")
     }
 })
+
+/* .env is used for declaring environment variables
+* and is only visible to local user */
 
 client.login(process.env.TOKEN)
