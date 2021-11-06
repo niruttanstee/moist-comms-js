6739e132a7a71f69b9b27b3c3cf7b14d8dfebb10
Nirutt <80709800+NiruttRP@users.noreply.github.com>
2021-11-06 22:42:12 +0000

    commit

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,8 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
+        name: "ping",
+        description: "Replies with Pong!",
     // put all functions in the execute() function
     async execute(message, args) {
-        message.channel.send("Pong!")
+        await message.channel.reply("Pong!")
     },
 };
