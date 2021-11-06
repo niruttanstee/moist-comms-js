fd323633a5515c82619990619ad1b255dc0b8e04
Nirutt <80709800+NiruttRP@users.noreply.github.com>
2021-11-06 17:55:35 +0000

    commit

--- startup.js
+++ startup.js
@@ -1,11 +1,15 @@
 require('dotenv').config(); //initialize dotenv
-const Discord = require('discord.js'); //import discord.js
-
-const client = new Discord.Client(); //create new client
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('messageCreate', message => {
+    if (message.content === 'ping') {
+        message.channel.send('pong');
+    }
+});
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using .env
