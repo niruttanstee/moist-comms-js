53bbcb45db6bb174146f1f73562b9d8f0a95df00
Nirutt <80709800+NiruttRP@users.noreply.github.com>
2021-11-08 01:36:38 +0000

    Update check.js

--- commands/check.js
+++ commands/check.js
@@ -2,13 +2,12 @@
 * This is to purely test if the database is functioning.
 * when someone says check it adds the user in the database and prints it out.
  */
-const { database_username, database_password } = require('./config.json');
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-    host: '0.0.0.0',
-    user: database_username,
-    password: database_password,
-    database: "s4_discord_moist_comms"
+    host: 'server.rapidshard.com',
+    user: "u4_qt3ntoxea2",
+    password: "U1FRSM2m2w0BwGZZD@98do.d",
+    database: "s4_discord"
 });
 
 // connect to the MySQL server
@@ -17,6 +16,7 @@ connection.connect(function(err) {
         return console.error('error: ' + err.message);
     }
 });
+const reee = []
 
 module.exports = {
     name: "check",
@@ -24,10 +24,23 @@ module.exports = {
     // put all functions in the execute() function
     async execute(message) {
         let author = message.author;
-        let sql = `INSERT INTO test (user) VALUES ("${author}")`;
-        connection.query(sql, function (err, result) {
+
+        let insertData = `INSERT INTO test (user) VALUES ("${author}")`;
+        connection.query(insertData, function (err, result) {
             if (err) throw err;
-            console.log(`1 record inserted: $`);
+            console.log(`1 record inserted`);
         });
+        connection.query(`SELECT * FROM test`, async function (err, result, fields) {
+            if (err) throw err;
+            for (let i = 0; i < result.length; i++) {
+                let x = result[i].user;
+                let y = result[i].id
+                reee.push(`${y}. ${x}`);
+            }
+            console.log(reee)
+            await message.channel.send(`${reee}`)
+        });
+
+
     },
 };
