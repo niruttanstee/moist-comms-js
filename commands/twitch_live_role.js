9d6844a6ab40faecf118097e76067004a818e34b
Nirutt <niruttsibutda@gmail.com>
2021-11-09 23:09:56 +0000

    Update twitch_live_role.js

--- commands/twitch_live_role.js
+++ commands/twitch_live_role.js
@@ -1,9 +1,4 @@
-/*
-* This is to purely test if the database is functioning.
-* when someone says check it adds the user in the database and prints it out.
- */
 const dayjs = require('dayjs');
-const {nowLiveRoleID} = require("./guild.json");
 
 module.exports = {
     name: "twitch_live_role",
