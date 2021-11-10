4ade6ac44edf2be0a105f2dc8b5721e388092107
Nirutt <niruttsibutda@gmail.com>
2021-11-10 23:07:35 +0000

    Update memberRoleVerify.js
    Changed member object which seems to work.

--- events/memberRoleVerify.js
+++ events/memberRoleVerify.js
@@ -2,12 +2,12 @@ const dayjs = require("dayjs");
 module.exports = {
     name: 'guildMemberUpdate',
     async execute(oldMember, newMember) {
-        if (newMember.pending && !oldMember.pending){
+        if (!newMember.pending && oldMember.pending){
+            console.log(`${newMember.pending}, ${oldMember.pending}`);
             let guild = newMember.guild;
-            let user = newMember.user;
             const {verifiedRoleID} = require('./guild.json');
             let role = guild.roles.cache.get(verifiedRoleID);
-            user.roles.add(role)
+            newMember.roles.add(role)
             console.log(`${dayjs()}: ${user.displayName} has accepted terms and conditions, giving ${role.name} role.`);
         }
 
