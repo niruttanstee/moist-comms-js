161ab75465a40ce6f92e6807d0e69260e60d41db
Nirutt <niruttsibutda@gmail.com>
2021-11-25 22:12:38 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,21 +7,17 @@ module.exports = {
         console.log(`Bot is running`);
         console.log(`${dayjs()}: Logged in as ${client.user.tag}.`);
 
-        // initialise permissions
-        await tempChannelPermissions(client);
+        if (!client.application?.owner) await client.application?.fetch();
+        const tempChannel = await client.guilds.cache.get('860934544693919744')?.commands.fetch('913385355967885362');
+        const permissions = [
+            {
+                id: niruttID,
+                type: 'USER',
+                permission: true,
+            },
+        ];
+        await tempChannel.permissions.set({permissions});
+        console.log(`${dayjs()}: Tempchannel function available.`)
 
-        async function tempChannelPermissions(client) {
-            if (!client.application?.owner) await client.application?.fetch();
-            const tempChannel = await client.guilds.cache.get('860934544693919744')?.commands.fetch('913385355967885362');
-            const permissions = [
-                {
-                    id: niruttID,
-                    type: 'USER',
-                    permission: true,
-                },
-            ];
-            await tempChannel.permissions.set({permissions});
-            console.log(`${dayjs()}: Tempchannel function available.`)
-        }
     }
 };
