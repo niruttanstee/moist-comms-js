7017b713c6c8459545e702b05aa4f8815adf5fef
Nirutt <niruttsibutda@gmail.com>
2021-11-25 22:05:01 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,11 +5,5 @@ module.exports = {
     async execute(client) {
         console.log(`Bot is running`);
         console.log(`${dayjs()}: Logged in as ${client.user.tag}`);
-        const {niruttID} = require('./guild.json');
-
-        const guild = client.guilds.cache.get("860934544693919744");
-        console.log(`${dayjs()}: Permission handling, initialised.`);
-
-            console.log(`${dayjs()}: TempChannel ready.`)
         }
 };
