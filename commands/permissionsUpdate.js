a35b01422e05179fef56d2f621e662e693be3345
Nirutt <niruttsibutda@gmail.com>
2021-11-25 22:39:43 +0000

    commit

--- commands/permissionsUpdate.js
+++ commands/permissionsUpdate.js
@@ -15,18 +15,34 @@ module.exports = {
 
     async execute(interaction) {
         const client = interaction.client;
-        const member = interaction.member;
+        const guildId = 860934544693919744;
 
         // initialise permissions
-        if (await tempChannelPermissions(client)) {
+        if (await tempChannelPermissions(client)
+            && await perms(client)) {
             await permissionUpdated();
         }
 
+        // permissions update permission properties
+        async function perms(client) {
+            if (!client.application?.owner) await client.application?.fetch();
+            const tempChannel = await client.guilds.cache.get(guildId)?.commands.fetch('913553831311314954');
+            const permissions = [
+                {
+                    id: niruttID,
+                    type: 'USER',
+                    permission: true,
+                },
+            ];
+            await tempChannel.permissions.set({permissions});
+            console.log(`${dayjs()}: Permission function's permission updated.`)
+            return true;
+        }
 
         // temporary channel permission properties
         async function tempChannelPermissions(client) {
             if (!client.application?.owner) await client.application?.fetch();
-            const tempChannel = await client.guilds.cache.get('860934544693919744')?.commands.fetch('913385355967885362');
+            const tempChannel = await client.guilds.cache.get(guildId)?.commands.fetch('913385355967885362');
             const permissions = [
                 {
                     id: niruttID,
@@ -46,7 +62,6 @@ module.exports = {
                 .setColor("#5bc04c")
                 .setTitle(`Permissions updated.`)
             await interaction.reply({embeds: [permissionUpdated]});
-            return console.log(`${dayjs()}: ${member.displayName} updated permissions.`)
         }
     },
 };
--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,18 @@
 const dayjs = require("dayjs");
-const {niruttID} = require("./guild.json");
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
         console.log(`Bot is running`);
         console.log(`${dayjs()}: Logged in as ${client.user.tag}.`);
+        const guilds = client.guilds;
+        const guild = await client.guilds.cache.get('860934544693919744');
+
+        guild.commands.fetch({ guild: "860934544693919744" })
+            .then(perms => console.log(perms))
+            .catch(console.error);
+
+        console.log( client.commands.keys());
+
     }
 };
