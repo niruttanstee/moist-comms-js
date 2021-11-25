c28376cf1a190d0cbd091c74cbd2024612edc84f
Nirutt <niruttsibutda@gmail.com>
2021-11-25 21:51:04 +0000

    Commit

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,37 @@
 const dayjs = require("dayjs");
+const {niruttID} = require("./guild.json");
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
         console.log(`Bot is running`)
         console.log(`${dayjs()}: Logged in as ${client.user.tag}`);
+        const {niruttID} = require('./guild.json');
+
+        const guild = client.guilds.cache.get("860934544693919744");
+        console.log(`${dayjs()}: Permission handling, initialised.`)
+        // call permissions handling
+        await tempChannelPermissions(client, guild);
+
+        async function tempChannelPermissions(client){
+            // permissions handling
+            if (!client.application?.owner) await client.application?.fetch();
+            const tempChannel = await client.guilds.cache.get('860934544693919744')?.commands.fetch('913385355967885362');
+            const permissions = [
+                {
+                    id: `${niruttID}`,
+                    type: 'USER',
+                    permission: true,
+                },
+            ];
+            await tempChannel.permissions.add({ permissions });
+            console.log(`${dayjs()}: TempChannel ready.`)
+        }
+
+
+        guild.commands.fetch()
+            .then(commands => console.log(commands))
+            .catch(console.error);
+        console.log(client.commands.keys())
     }
 };
