9c28df94cf56e4dfeb2d25ca0b250581c078fb12
Nirutt <niruttsibutda@gmail.com>
2022-01-12 01:29:02 +0000

    Redeemable Full Function

--- events/scheduler.js
+++ events/scheduler.js
@@ -44,67 +44,62 @@ async function redeemableSchedule(client, guild) {
             const messageId = result[i].messageId;
             let giveawayDate = result[i].date;
             let dateSplit = giveawayDate.split("/")
-            const date = new Date(dateSplit[0], dateSplit[1] - 1, dateSplit[2], dateSplit[3], dateSplit[4]);
-            try {
-                const job = await schedule.scheduleJob(date, async function () {
+            let alreadyWinner = result[i].winner;
+            if (alreadyWinner == null) {
+                const date = new Date(dateSplit[0], dateSplit[1] - 1, dateSplit[2], dateSplit[3], dateSplit[4]);
+                try {
+                    const job = await schedule.scheduleJob(date, async function () {
+                        const channelId  = result[i].publishedChannelId;
+                        const channel = await guild.channels.fetch(channelId);
+                        const publishedMessageId = result[i].publishedMessageId;
+                        const postedAnnouncementMessage = await channel.messages.fetch(publishedMessageId)
+                            .then(message => {
+                                return message})
+                            .catch(console.error);
+                        await postedAnnouncementMessage.reactions.removeAll();
 
-                    database.query("SELECT * FROM redeemable", async function (err, result, fields) {
-                        if (err) throw err;
-                        for (let i = 0; i < result.length; i++) {
-                            if (messageId === result[i].messageId) {
-                                const channelId  = result[i].publishedChannelId;
-                                const channel = await guild.channels.fetch(channelId);
-                                const publishedMessageId = result[i].publishedMessageId;
-                                const postedAnnouncementMessage = await channel.messages.fetch(publishedMessageId)
-                                    .then(message => {
-                                        return message})
-                                    .catch(console.error);
-                                await postedAnnouncementMessage.reactions.removeAll();
+                        // pick a winner
+                        const participants = (result[i].participants).split(",");
 
-                                // pick a winner
-                                const participants = (result[i].participants).split(",");
+                        let member = guild.members.cache.get(result[i].ownerId);
+                        let gameName = result[i].gameName;
+                        let redeemableType = result[i].redeemableType;
+                        let DlcName = result[i].DlcName;
+                        let ImageLink = result[i].imageLink;
+                        let platform = result[i].platform;
+                        let key = result[i].skey;
+                        let removeDate = result[i].removeDate;
+                        // remove reaction
+                        try {
+                            const winnerNum = Math.floor(Math.random() * participants.length);
+                            const winner = participants[winnerNum];
+                            const winnerMember = await guild.members.fetch(winner)
+                                .then(member => {return member})
+                                .catch(console.error);
 
-                                let member = guild.members.cache.get(result[i].ownerId);
-                                let gameName = result[i].gameName;
-                                let redeemableType = result[i].redeemableType;
-                                let DlcName = result[i].DlcName;
-                                let ImageLink = result[i].imageLink;
-                                let platform = result[i].platform;
-                                let key = result[i].skey;
-                                let removeDate = result[i].removeDate;
-                                // remove reaction
-                                try {
-                                    const winnerNum = Math.floor(Math.random() * participants.length);
-                                    const winner = participants[winnerNum];
-                                    const winnerMember = await guild.members.fetch(winner)
-                                        .then(member => {return member})
-                                        .catch(console.error);
+                            let messageId = result[i].messageId;
 
-                                    let messageId = result[i].messageId;
-
-                                    // update database
-                                    let sql = `UPDATE redeemable SET winner = ${winnerMember.user.id} WHERE messageId = ${messageId}`;
-                                    database.query(sql, function (err, result) {
-                                            if (err) throw err;
-                                            console.log(`${dayjs()}: winner updated.`);
-                                        }
-                                    );
-
-                                    // update published message embed
-
-                                    await updateEmbedMessage(member, channel, guild, gameName, redeemableType, DlcName, ImageLink, giveawayDate, postedAnnouncementMessage, platform, winnerMember);
-                                    // tell winner
-                                    await winnerEmbed(member, channel, guild, gameName,redeemableType,DlcName,ImageLink,giveawayDate, postedAnnouncementMessage,platform,winnerMember,key);
-                                    // schedule remove date
-                                    return await redeemableScheduleRemoval(client, removeDate, channel);
-                                } catch (e) {
-                                    console.error(e)
+                            // update database
+                            let sql = `UPDATE redeemable SET winner = ${winnerMember.user.id} WHERE messageId = ${messageId}`;
+                            database.query(sql, function (err, result) {
+                                    if (err) throw err;
+                                    console.log(`${dayjs()}: winner updated.`);
                                 }
-                            }
+                            );
+
+                            // update published message embed
+
+                            await updateEmbedMessage(member, channel, guild, gameName, redeemableType, DlcName, ImageLink, giveawayDate, postedAnnouncementMessage, platform, winnerMember);
+                            // tell winner
+                            await winnerEmbed(member, channel, guild, gameName,redeemableType,DlcName,ImageLink,giveawayDate, postedAnnouncementMessage,platform,winnerMember,key);
+                            // schedule remove date
+                            return await redeemableScheduleRemoval(client, removeDate, channel);
+                        } catch (e) {
+                            console.error(e)
                         }
                     });
-                });
-            } catch (e) {console.error(e)}
+                } catch (e) {console.error(e)}
+            }
         }
     });
 }
